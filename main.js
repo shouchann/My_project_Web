5fca17336d41ec0a3a801c9db23d306101357bfb
shouchann <57974233+shouchann@users.noreply.github.com>
2021-02-03 15:27:42 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+window.addEventListener('DOMContentLoaded', function(){
+  $('.menu-btn').on('click', function(){
+    $('.menu').toggleClass('is-active');
+  });
+});
+
+
+window.addEventListener('DOMContentLoaded', function(){
+  $('.question').on('click', function(){
+    $('.question').toggleClass('active');
+  })
+})
+
